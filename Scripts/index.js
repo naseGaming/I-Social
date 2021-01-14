df1e028ac1605ee350b2daf849d3d0e1a904f1b2
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-01-14 20:03:08 +0800

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,4 +2,18 @@ $(document).ready(function () {
 
 	$("#frmRegister").hide();
 
+    $("#forget").click(function () {
+		$("#frmRegister").hide();
+		$("#frmLogin").hide();
+    });
+
+    $("#create").click(function () {
+		$("#frmLogin").hide();
+		$("#frmRegister").show();
+    });
+
+    $("#log").click(function () {
+		$("#frmRegister").hide();
+		$("#frmLogin").show();
+    });
 });
