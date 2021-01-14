8d3f82272be151cb76bd7a36d8e47b3be7b2e659
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-01-15 02:24:21 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="Design/favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="Design/home.css" />
+		<script src="https://kit.fontawesome.com/8b6b1fa9e8.js" crossorigin="anonymous"></script>
+        <script src = "Scripts/jquery-3.1.1.min.js"></script>
+        <script src = "Scripts/jquery-ui.min.js"></script>
+		<script src = "Scripts/index.js" ></script>
+	</head>
+	<body>
+		<div id = "left-bar" >
+		</div>
+		<div id = "top-bar" >
+			<textarea id = "postText" class = "postText" rows = "4" cols = "50" placeholder = "Open up a discussion"></textarea>
+			<button id = "postBtn" class = "postBtn" >Post</button>
+		</div>
+		<div id = "right-bar" >
+		</div>
+	</body>
+</html>
