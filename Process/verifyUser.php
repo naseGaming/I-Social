495b6fcc79863cad61fb7e7898094b3210582077
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-01-15 10:46:06 +0800

    Update verifyUser.php

--- Process/verifyUser.php
+++ Process/verifyUser.php
@@ -1,10 +1,13 @@
 <?php
+	//php file for verifying the user
 	$username = $_POST['user'];
+	//gets the ip address of the user
 	$ipAdd = $_SERVER['REMOTE_ADDR'];  
 	$flag = false;
 
 	require 'config.php';
 
+	//checks if the username is existing in the database
 	$sql = mysqli_query($con,"SELECT * FROM `users` where `Username` = '$username' ");
 
 
@@ -18,6 +21,7 @@
 		echo "Error Sql 1";
 	}
 
+	//if existing saves the ip address of the user for future verifications
 	if($flag){
 		$sql2 = mysqli_query($con,"INSERT INTO `login_data`(`Ip_Address`, `Users_Id`) VALUES ('$ipAdd','$id')");
 
