c4eb22913752e8531889d7661a7a5d472cd05733
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-01-15 10:45:46 +0800

    Update signUp.php

--- Process/signUp.php
+++ Process/signUp.php
@@ -1,4 +1,5 @@
 <?php
+	//php file for register
 	$username = $_POST['user'];
     $password = $_POST['pass'];
     $first = $_POST['first'];
@@ -11,11 +12,24 @@
 	
 	require 'config.php';
 
+	//checks if the username is already existing in the database
 	$sql = mysqli_query($con,"SELECT * FROM `users` where `Username` = '$username' ");
 
 	if($sql){
 		while($row = mysqli_fetch_array($sql)){
 			$userFlag = true;
+		}
+	}
+	else{
+		echo "Error Sql 1";
+	}
+
+	//checks if the email is already existing in the database
+	$sql2 = mysqli_query($con,"SELECT * FROM `users` where `Email` = '$email' ");
+
+	if($sql2){
+		while($row2 = mysqli_fetch_array($sql2)){
+			$emailFlag = true;
 		}
 	}
 	else{
@@ -30,7 +44,7 @@
 			echo "Email";
 		}
 		else{
-
+			//if both username and email is non existent in the database saves the credentials of the user
 			$sql3 = mysqli_query($con,"INSERT INTO `users`(`Username`, `Password`, `First_Name`, `Middle_Name`, `Last_Name`, `Email`) VALUES ('$username','$hashed_password','$first','$mid','$last','$email')");
 					
 			if($sql3){
