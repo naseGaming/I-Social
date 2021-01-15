90daa771b02ca5b6525b506341339cab7cfe67ae
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-01-15 10:12:20 +0800

    Update signIn.php

--- Process/signIn.php
+++ Process/signIn.php
@@ -1,36 +1,97 @@
 <?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+	session_start();
+
 	$username = $_POST['user'];
     $password = $_POST['pass'];
-	$flag = false;
-	
-	$xml = new DOMDocument;
-	$xml->load('../Data/users.xml');
+    $code = $_POST['code'];
 
-	$users = $xml->getElementsByTagName('user');
-	$ctr = 0;
 	$flag = false;
+	$ip = false;
+	$ipAdd = $_SERVER['REMOTE_ADDR'];  
 	
-	while($ctr < $users->length){
-		$user = $users->item($ctr)->getElementsByTagName('username')->item(0)->nodeValue;
-		$id = $users->item($ctr)->getElementsByTagName('id')->item(0)->nodeValue;
-		$pass = $users->item($ctr)->getElementsByTagName('password')->item(0)->nodeValue;
-		
-		if($username == $user){
+	require 'config.php';
+	require 'PHPMailer.php';
+	require 'Exception.php';
+	require 'SMTP.php';
+
+	$sql = mysqli_query($con,"SELECT * FROM `users` where `Username` = '$username' ");
+
+	if($sql){
+		while($row = mysqli_fetch_array($sql)){
+			$id = $row['Id'];
+			$hash = $row['Password'];
+			$status = $row['Status'];
+			$first = $row['First_Name'];
+			$last = $row['Last_Name'];
+			$email = $row['Email'];
+
 			$flag = true;
-			break;
 		}
-		$ctr++;
-	}
-	
-	if($flag){
-		if (password_verify($password, $pass)){
-			echo $id;
+
+		if(!$flag){
+			echo "User";
 		}
 		else{
-			echo "pass";
+			if (password_verify($password, $hash)){
+				if($status === 0){
+					echo $id."/".$first."/".$last;
+				}
+				else{
+					$sql2 = mysqli_query($con,"SELECT * FROM `login_data` where `Ip_Address` = '$ipAdd' ");
+
+					if($sql2){
+						while($row2 = mysqli_fetch_array($sql2)){
+							$login_id = $row['Users_Id'];
+							$ip = true;
+						}
+
+						if($ip){
+							if($id == $login_id){
+								echo $id."/".$first."/".$last;
+							}
+							else{
+								//if(sendMail($email, $code)){
+									echo "Verify";
+								//}
+							}
+						}
+						else{
+							//if(sendMail($email, $code)){
+								echo "Verify";
+							//}
+						}
+					}
+				}
+			}
+			else{
+				echo "Pass";
+			}
 		}
 	}
 	else{
-		echo "user";
+		echo "Error Sql 1";
+	}
+
+	function sendMail($email, $code){
+		$mail = new PHPMailer();
+
+		$mail->setFrom("isocialnoreply2021@gmail.com");
+
+		$mail->addAddress($email);
+
+		$mail->Subject = "Verify User";
+
+		$mail->Body = "This is your code ".$code;
+
+		if (!$mail->send())
+		{
+		   echo $mail->ErrorInfo;
+		   return false;
+		}
+		else{
+			return true;
+		}
 	}
 ?>
