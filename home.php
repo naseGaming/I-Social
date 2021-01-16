6a17cd289ce9f0574097b77f7a77d7c37ee4c70f
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-01-16 22:27:34 +0800

    Update home.php

--- home.php
+++ home.php
@@ -8,14 +8,24 @@
 		<script src="https://kit.fontawesome.com/8b6b1fa9e8.js" crossorigin="anonymous"></script>
         <script src = "Scripts/jquery-3.1.1.min.js"></script>
         <script src = "Scripts/jquery-ui.min.js"></script>
-		<script src = "Scripts/index.js" ></script>
+		<script src = "Scripts/home.js" ></script>
 	</head>
 	<body>
 		<div id = "left-bar" >
+			<div id = "profilePicture">
+			</div>
+			<a id = "userName" >
+			</a>
 		</div>
 		<div id = "top-bar" >
 			<textarea id = "postText" class = "postText" rows = "4" cols = "50" placeholder = "Open up a discussion"></textarea>
+			<a>
+			<!-- 
+				<button id = "agreeBtn" class = "reactBtn" ><i id = "reacts" class="fas fa-thumbs-up"></i></button>
+				<button id = "disagreeBtn" class = "reactBtn2" ><i id = "reacts2" class="fas fa-thumbs-down"></i></button>
+			-->
 			<button id = "postBtn" class = "postBtn" >Post</button>
+			</a>
 		</div>
 		<div id = "right-bar" >
 		</div>
