49a7824868b186f0be65942c7c72009aabc22451
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-01-17 08:00:41 +0800

    Update home.php

--- home.php
+++ home.php
@@ -21,11 +21,11 @@
 			<br>
 			<br>
 			<ul>
-				<li class = "left-bar-items"><a class="inactives"><i class="fas fa-user-alt"></i> Profile</a></li>
-				<li class = "left-bar-items"><a class="inactives"><i class="fas fa-comments"></i> Messages</a></li>
-				<li class = "left-bar-items"><a class="inactives"><i class="fas fa-bell"></i> Notifications</a></li>
-				<li class = "left-bar-items"><a class="inactives"><i class="fas fa-cogs"></i> Settings</a></li>
-				<li class = "left-bar-items"><a class="inactives"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+				<li class = "left-bar-items"><a class = "inactives" id = "left-profile"><i class="fas fa-user-alt"></i> Profile</a></li>
+				<li class = "left-bar-items"><a class = "inactives" id = "left-messages"><i class="fas fa-comments"></i> Messages</a></li>
+				<li class = "left-bar-items"><a class = "inactives" id = "left-notfications"><i class="fas fa-bell"></i> Notifications</a></li>
+				<li class = "left-bar-items"><a class = "inactives" id = "left-settings"><i class="fas fa-cogs"></i> Settings</a></li>
+				<li class = "left-bar-items"><a class = "inactives" id = "left-logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
 			</ul>
 		</div>
 		<div>
@@ -43,6 +43,10 @@
 			</div>
 		</div>
 		<div id = "right-bar" >
+			<a>
+				<i id = "search-icon" class="fas fa-search"></i>
+				<input type = "text" placeholder="Search" id = "search" class = "search" />
+			</a>
 		</div>
 	</body>
 </html>
