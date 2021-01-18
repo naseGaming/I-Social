a92a1e7eae545fc17f25744e534599a548c237cc
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-01-18 10:05:38 +0800

    Update home.js

--- Scripts/home.js
+++ Scripts/home.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 	controller.showName(model, view);
 	controller.checkStatus(model, view);
 	controller.getPosts(model, view);
+	controller.hideDivs(view);
 
     $("#left-logout").click(function () {
     	controller.logout(model, view);
@@ -66,6 +67,18 @@ class HomeView{
 
 	showRemainingNumber(data){
 		document.getElementById("charactersLeft").innerHTML = data;
+	}
+
+	hideSuccess(){
+		$("#successPost").hide();
+	}
+
+	showSuccess(data){
+		document.getElementById("successPost").innerHTML = data;
+		$("#successPost").slideDown();
+		setTimeout(function(){ 
+			$("#successPost").slideUp(); 
+		}, 2000);
 	}
 }
 
@@ -138,25 +151,52 @@ class HomeModel{
 			}
 		});
 	}
+
+	reactPostProcess(postId, id, reaction){
+        return $.ajax({
+			type: "POST",
+			url: '/ISocial/Process/reactPost.php',
+			data: jQuery.param({ postId: postId, id: id, reaction: reaction }),
+			contentType: "application/x-www-form-urlencoded; charset=utf-8",
+			success: function (responses) {
+			},
+			error: function () {
+			}
+		});
+	}
+
+	saveActivity(id, data){
+        return $.ajax({
+			type: "POST",
+			url: '/ISocial/Process/activityHistory.php',
+			data: jQuery.param({ id: id, data: data }),
+			contentType: "application/x-www-form-urlencoded; charset=utf-8",
+			success: function (responses) {
+			},
+			error: function () {
+			}
+		});
+	}
 }
 
 class HomeController{
-	constructor(name, id, postData, charResult, postId){
+	constructor(name, id, postData, charResult, postId, reactClass){
 		this._name = name;
 		this._id = id;
 		this.postData = postData;
 		this._charResult = charResult;
 		this.postId = postId;
+		this.reactClass = reactClass;
 	}
 
 	showName(model, view){
 		this._name = model.showNameProcess();
-		this._id = model.getIdProcess();
 
 		view.displayName(this._name);
 	}
 
 	logout(model, view){
+		this._id = model.getIdProcess();
 		$.when(model.logoutProcess(this._id)).done(function (result){
 			if(result == "Success"){
 				sessionStorage.clear();
@@ -169,9 +209,11 @@ class HomeController{
 	}
 
 	newPost(model, view){
+		this._id = model.getIdProcess();
 		$.when(model.newPostProcess(this._id, this.postData)).done(function (result){
 			if(result == "Success"){
 				view.clearNewPost();
+				view.showSuccess("Posted Successfully <i class='fas fa-check-circle'></i>");
 			}
 			else{
 				view.logError(result);
@@ -180,15 +222,18 @@ class HomeController{
 		$.when(model.getPostsProcess(this._id)).done(function (result){
 			view.showPosts(result);
 		});
+		model.saveActivity(this._id, "Posted/"+this.postData);
 	}
 
 	checkStatus(model, view){
+		this._id = model.getIdProcess();
 		if(this._id === ""){
 			view.goLogout();
 		}
 	}
 
 	getPosts(model, view){
+		this._id = model.getIdProcess();
 		$.when(model.getPostsProcess(this._id)).done(function (result){
 			view.showPosts(result);
 		});
@@ -224,6 +269,56 @@ class HomeController{
 		this._id = model.getIdProcess();
 		$.when(model.deletePostProcess(this.postId)).done(function (result){
 			view.logError(result);
+			view.showSuccess("Deleted Successfully <i class='fas fa-check-circle'></i>");
+		});
+		$.when(model.getPostsProcess(this._id)).done(function (result){
+			view.showPosts(result);
+		});
+		model.saveActivity(this._id, "Deleted the post/"+this.postId);
+	}
+
+	hideDivs(view){
+		view.hideSuccess();
+	}
+
+	agreePost(model, view){
+		this._id = model.getIdProcess();
+		$.when(model.reactPostProcess(this.postId, this._id, "1")).done(function (result){
+			if(result == "Reacted"){
+				//success
+				model.saveActivity(this._id, "Agreed to the post/"+this.postId);
+			}
+			else if(result == "Deleted"){
+				model.saveActivity(this._id, "Removed reaction to the post/"+this.postId);
+			}
+			else if(result == "Edited"){
+				model.saveActivity(this._id, "Changed reaction to agree the post/"+this.postId);			
+			}
+			else{
+				view.logError(result);
+			}
+		});
+		$.when(model.getPostsProcess(this._id)).done(function (result){
+			view.showPosts(result);
+		});
+	}
+
+	disAgreePost(model, view){
+		this._id = model.getIdProcess();
+		$.when(model.reactPostProcess(this.postId, this._id, "0")).done(function (result){
+			if(result == "Reacted"){
+				//success
+				model.saveActivity(this._id, "Agreed to the post/"+this.postId);
+			}
+			else if(result == "Deleted"){
+				model.saveActivity(this._id, "Removed reaction to the post/"+this.postId);
+			}
+			else if(result == "Edited"){
+				model.saveActivity(this._id, "Changed reaction to disagree the post/"+this.postId);			
+			}
+			else{
+				view.logError(result);
+			}
 		});
 		$.when(model.getPostsProcess(this._id)).done(function (result){
 			view.showPosts(result);
@@ -241,7 +336,25 @@ function deletePost(app){
 
 	controller.postId = app.id;
 
-	controller.checkStatus(model, view);
-
 	controller.deletePost(model, view);
+}
+
+function agreePost(app){
+	var view = new HomeView;
+	var model = new HomeModel;
+	var controller = new HomeController;
+
+	controller.postId = app.name;
+
+	controller.agreePost(model, view);
+}
+
+function disAgreePost(app){
+	var view = new HomeView;
+	var model = new HomeModel;
+	var controller = new HomeController;
+
+	controller.postId = app.name;
+
+	controller.disAgreePost(model, view);
 }
