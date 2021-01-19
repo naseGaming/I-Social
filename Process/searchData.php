8449f07b83449719a58a7bc6648fbfc69bfcf2a8
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-01-19 10:38:53 +0800

    Update searchData.php

--- Process/searchData.php
+++ Process/searchData.php
@@ -1,9 +1,15 @@
 <?php
 	$id = $_POST['id'];
 	$searchData = $_POST['searchData'];
+	$searchData = strtolower($searchData);
+	$dataPieces = str_split($searchData);
+	$length = count($dataPieces);
 	$flag = false;
 	$searchResult = false;
+	$seemore = false;
 	$searchCounter = 0;
+	$first = false;
+	$mid = false;
 
 	require 'config.php';
 
@@ -23,38 +29,67 @@
 
 		if($sql2){
 			while($row2=mysqli_fetch_assoc($sql2)){
+				$first = false;
+				$mid = false;
+
 				$searchId = $row2['Id'];
 				$firstname = $row2['First_Name'];
 				$midname = $row2['Middle_Name'];
 				$lastname = $row2['Last_Name'];
 
-				if($searchData == $firstname){
-					$searchResult = true;
+				$firstname = strtolower($firstname);
+				$firstnamePieces = str_split($firstname, $length);
+				$firstnameLength = count($firstnamePieces);
+
+				$midname = strtolower($midname);
+				$midnamePieces = str_split($midname, $length);
+				$midnameLength = count($midnamePieces);
+
+				$lastname = strtolower($lastname);
+				$lastnamePieces = str_split($lastname, $length);
+				$lastnameLength = count($lastnamePieces);
+
+				if($searchData == $firstnamePieces[0]){
 					$searchCounter++;
-				}
-				else if($searchData == $midname){
 					$searchResult = true;
-					$searchCounter++;
-				}
-				else if($searchData == $lastname){
-					$searchResult = true;
-					$searchCounter++;
 				}
 				else{
-					$searchResult = false;
+					if($searchData == $midnamePieces[0]){
+						$searchCounter++;
+						$searchResult = true;
+					}
+					else{
+						if($searchData == $lastnamePieces[0]){
+							$searchCounter++;
+							$searchResult = true;
+						}
+						else{
+							$searchResult = false;
+						}
+					}
 				}
 
-				if($searchResult){
-					echo "<a class = 'searchItems' onClick = 'goTo(this);' id = '".$searchId."'>
+				if($searchCounter > 5){
+					$seemore = true;
+				}
+				else{
+					if($searchResult){
+						echo "<div class = 'searchItems' onClick = 'goTo(this);' id = '".$searchId."'>
 							".$firstname." ".$lastname."
-						</a>";
+						</div>";
+					}
 				}
 			}
 
 			if($searchCounter <= 0){
-				echo "<a class = 'searchItems' >
-				No Result Found!
-					</a>";
+				echo "<div class = 'searchItems' >
+					No Result Found!
+				</div>";
+			}
+			if($seemore){
+				echo "<div class = 'searchItems' onClick = 'goTo(this);' id = 'seemore'>
+					See more about ".$searchData."
+				</div>";
 			}
 		}
 		else{
