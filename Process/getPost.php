e22affe7bdff5647028f1fc08bd3c7ab8b9f9ef8
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-01-21 04:03:16 +0800

    Update getPost.php

--- Process/getPost.php
+++ Process/getPost.php
@@ -1,7 +1,7 @@
 <?php
 	$id = $_POST['id'];
+	$data = $_POST['data'];
 	$flag =  false;
-	$flag2 = false;
 	$reactCtr = 0;
 	$agreeCtr = 0;
 	$disagreeCtr = 0;
@@ -9,7 +9,7 @@
 	$reaction = "";
 	date_default_timezone_set("Asia/Manila");
 
-	require 'config.php';
+	require 'getFriendList.php';
 
 	$sql = mysqli_query($con,"SELECT * FROM `users` where `Id` = '$id'");
 
@@ -24,100 +24,126 @@
 		echo "Error Sql 1";
 	}
 
+	$name = array($first, $last);
+
 	if($flag){
-		$sql2 = mysqli_query($con,"SELECT * FROM `posts` where `Post_Sender` = '$id' order by `POST_Id` desc");
+		$sql2 = mysqli_query($con,"SELECT * FROM `posts` order by `POST_Id` desc");
 
 		if($sql2){
 			while($row2=mysqli_fetch_assoc($sql2)){
-				$flag2 = true;
+				$flag2 = false;
+				$Post_Sender = $row2['Post_Sender'];
 				$reactCtr = 0;
 				$agreeCtr = 0;
 				$disagreeCtr = 0;
 				$isReacted = false;
 				$post_id = $row2['Post_Id'];
 
-				$sql3 = mysqli_query($con,"SELECT * FROM `reactions` where `Post_Id` = '$post_id' order by `Reaction` desc");
+				if($Post_Sender == $id){
+					$flag2 = true;
+					$first = $name[0];
+					$last = $name[1];
+				}
 
-				if($sql3){
-					while($row3=mysqli_fetch_assoc($sql3)){
-						$reactCtr++;
+				if($data == "newsfeed"){
+					if($listData == "full"){
+						$listCounter = count($list);
 
-						if($row3['Reaction'] == "0"){
-							$disagreeCtr++;
-							$reaction = "Disagree";
-						}
-						else{
-							$agreeCtr++;
-							$reaction = "Agree";
-						}
-
-						if($row3['User_Id'] == $id){
-							$isReacted = true;
+						for($x = 1; $x < $listCounter; $x = $x + 3){
+							if($list[$x] == $Post_Sender){
+								$flag2 = true;
+								$first = $list[$x+1];
+								$last = $list[$x+2];
+							}
 						}
 					}
 				}
-				else{
-					echo "Error Sql 3";
-				}
 
-				if($reactCtr > 0){
-					echo "<div class = 'posts'>
-						<div class = 'userName'>
-							<img class = 'usersPicture' src = 'Design/Resources/Default.png' alt = '".$first."-".$last."' >
-							<a class = 'aName' onClick = 'goProfile(this);' id = ".$row2["Post_Sender"].">".$first." ".$last."</a> 
-							<button onClick = 'deletePost(this);' class = 'deleteBtn' id = ".$row2['Post_Id'].">
-								<i class='fas fa-trash'></i>
-							</button>
-						</div>
-						<div id = '".$row2['Post_Id']."-text' class = 'postData'>
-							".$row2['Post_Data']."
-						</div>";
-					if($reaction == "Agree"){
-						echo "<button id = 'agreeBtn' name = '".$row2['Post_Id']."' onClick = 'agreePost(this);' class = 'reactBtnAct' >
-							<i id = 'reacts' class='fas fa-thumbs-up'></i> ".$agreeCtr."
-						</button>
-						<button id = 'disagreeBtn' name = '".$row2['Post_Id']."' onClick = 'disAgreePost(this);' class = 'reactBtn2' >
-							<i id = 'reacts2' class='fas fa-thumbs-down'></i> ".$disagreeCtr."
-						</button>";
+
+				if($flag2){
+					$sql3 = mysqli_query($con,"SELECT * FROM `reactions` where `Post_Id` = '$post_id' order by `Reaction` desc");
+
+					if($sql3){
+						while($row3=mysqli_fetch_assoc($sql3)){
+							$reactCtr++;
+
+							if($row3['Reaction'] == "0"){
+								$disagreeCtr++;
+								$reaction = "Disagree";
+							}
+							else{
+								$agreeCtr++;
+								$reaction = "Agree";
+							}
+
+							if($row3['User_Id'] == $id){
+								$isReacted = true;
+							}
+						}
 					}
 					else{
-						echo "<button id = 'agreeBtn' name = '".$row2['Post_Id']."' onClick = 'agreePost(this);' class = 'reactBtn' >
-							<i id = 'reacts' class='fas fa-thumbs-up'></i> ".$agreeCtr."
-						</button>
-						<button id = 'disagreeBtn' name = '".$row2['Post_Id']."' onClick = 'disAgreePost(this);' class = 'reactBtn2Act' >
-							<i id = 'reacts2' class='fas fa-thumbs-down'></i> ".$disagreeCtr."
-						</button>";
+						echo "Error Sql 3";
 					}
-					echo "<button id = 'replyBtn' class = 'replyBtn' >
-							<i id = 'reacts2' class='fas fa-reply-all'></i>
-						</button>
-						<a class = 'postDate'>".$row2['Post_Date']."</a>
-					</div>";
-				}
-				else{
-					echo "<div class = 'posts'>
-						<div class = 'userName'>
-							<img class = 'usersPicture' src = 'Design/Resources/Default.png' alt = '".$first."-".$last."' >
-							<a class = 'aName' onClick = 'goProfile(this);' id = ".$row2["Post_Sender"].">".$first." ".$last."</a> 
-							<button onClick = 'deletePost(this);' class = 'deleteBtn' id = ".$row2['Post_Id'].">
-								<i class='fas fa-trash'></i>
+
+					if($reactCtr > 0){
+						echo "<div class = 'posts'>
+							<div class = 'userName'>
+								<img class = 'usersPicture' src = 'Design/Resources/Default.png' alt = '".$first."-".$last."' >
+								<a class = 'aName' onClick = 'goProfile(this);' id = ".$row2["Post_Sender"].">".$first." ".$last."</a> 
+								<button onClick = 'deletePost(this);' class = 'deleteBtn' id = ".$row2['Post_Id'].">
+									<i class='fas fa-trash'></i>
+								</button>
+							</div>
+							<div id = '".$row2['Post_Id']."-text' class = 'postData'>
+								".$row2['Post_Data']."
+							</div>";
+						if($reaction == "Agree"){
+							echo "<button id = 'agreeBtn' name = '".$row2['Post_Id']."' onClick = 'agreePost(this);' class = 'reactBtnAct' >
+								<i id = 'reacts' class='fas fa-thumbs-up'></i> ".$agreeCtr."
 							</button>
-						</div>
-						<div id = '".$row2['Post_Id']."-text' class = 'postData'>
-							".$row2['Post_Data']."
-						</div>
-						<a>
-						<button id = 'agreeBtn' name = '".$row2['Post_Id']."' onClick = 'agreePost(this);' class = 'reactBtn' >
-							<i id = 'reacts' class='fas fa-thumbs-up'></i></button>
-						<button id = 'disagreeBtn' name = '".$row2['Post_Id']."' onClick = 'disAgreePost(this);' class = 'reactBtn2' >
-							<i id = 'reacts2' class='fas fa-thumbs-down'></i>
-						</button>
-						<button id = 'replyBtn' class = 'replyBtn' >
-							<i id = 'reacts2' class='fas fa-reply-all'></i>
-						</button>
-						<a class = 'postDate'>".$row2['Post_Date']."</a>
-						</a>
-					</div>";
+							<button id = 'disagreeBtn' name = '".$row2['Post_Id']."' onClick = 'disAgreePost(this);' class = 'reactBtn2' >
+								<i id = 'reacts2' class='fas fa-thumbs-down'></i> ".$disagreeCtr."
+							</button>";
+						}
+						else{
+							echo "<button id = 'agreeBtn' name = '".$row2['Post_Id']."' onClick = 'agreePost(this);' class = 'reactBtn' >
+								<i id = 'reacts' class='fas fa-thumbs-up'></i> ".$agreeCtr."
+							</button>
+							<button id = 'disagreeBtn' name = '".$row2['Post_Id']."' onClick = 'disAgreePost(this);' class = 'reactBtn2Act' >
+								<i id = 'reacts2' class='fas fa-thumbs-down'></i> ".$disagreeCtr."
+							</button>";
+						}
+						echo "<button id = 'replyBtn' class = 'replyBtn' >
+								<i id = 'reacts2' class='fas fa-reply-all'></i>
+							</button>
+							<a class = 'postDate'>".$row2['Post_Date']."</a>
+						</div>";
+					}
+					else{
+						echo "<div class = 'posts'>
+							<div class = 'userName'>
+								<img class = 'usersPicture' src = 'Design/Resources/Default.png' alt = '".$first."-".$last."' >
+								<a class = 'aName' onClick = 'goProfile(this);' id = ".$row2["Post_Sender"].">".$first." ".$last."</a> 
+								<button onClick = 'deletePost(this);' class = 'deleteBtn' id = ".$row2['Post_Id'].">
+									<i class='fas fa-trash'></i>
+								</button>
+							</div>
+							<div id = '".$row2['Post_Id']."-text' class = 'postData'>
+								".$row2['Post_Data']."
+							</div>
+							<a>
+							<button id = 'agreeBtn' name = '".$row2['Post_Id']."' onClick = 'agreePost(this);' class = 'reactBtn' >
+								<i id = 'reacts' class='fas fa-thumbs-up'></i></button>
+							<button id = 'disagreeBtn' name = '".$row2['Post_Id']."' onClick = 'disAgreePost(this);' class = 'reactBtn2' >
+								<i id = 'reacts2' class='fas fa-thumbs-down'></i>
+							</button>
+							<button id = 'replyBtn' class = 'replyBtn' >
+								<i id = 'reacts2' class='fas fa-reply-all'></i>
+							</button>
+							<a class = 'postDate'>".$row2['Post_Date']."</a>
+							</a>
+						</div>";
+					}
 				}
 			}
 		}
